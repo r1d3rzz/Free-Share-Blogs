e125628efa0c92b823eefa7e9961dd88814914e1
rider <myintthwaykhine127@gmail.com>
2022-08-27 11:37:23 +0630

    user login out signup routes cover with middlewate

--- routes/web.php
+++ routes/web.php
@@ -25,11 +25,11 @@ Route::controller(BlogController::class)->group(function(){
 
 //for User Login/out and SignUp Routes
 Route::controller(UserController::class)->group(function(){
-    Route::get('/user/login','login');
-    Route::get('/user/logout','logout');
-    Route::get('/user/signup','signup');
-    Route::post('/user/register','register');
-    Route::post('/user/post_login','post_login');
+    Route::get('/user/login','login')->middleware('guest')->name('login');
+    Route::get('/user/logout','logout')->middleware('auth');
+    Route::get('/user/signup','signup')->middleware('guest');
+    Route::post('/user/register','register')->middleware('guest');
+    Route::post('/user/post_login','post_login')->middleware('guest');
 });
 
 //for User Comments
